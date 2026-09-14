51260709ff5efd341360b171367323ac8ec52434
omar <abdelazizomar899@gmail.com>
2026-09-14 20:11:02 +0300

    finish gauges function

--- Drivers_codes/HAL/GAUGES/gauges.c
+++ Drivers_codes/HAL/GAUGES/gauges.c
@@ -44,4 +44,57 @@ void GAU_Init(void) {
     /* Initialize ADC with AREF reference and Prescaler 64[cite: 1, 4] */
     ADC_Init(ADC_REF_AREF, ADC_PRESC_64);
 }
+void GAU_Update(CarData_t *CarData) {
+    uint16 raw_fuel, raw_coolant, raw_batt, raw_oil;
+    uint32 sum_fuel = 0, sum_coolant = 0;
+    uint8 i;
+    
+    /* Read channels (Using fast polling for simplicity, real system might use interrupts) */
+    ADC_ReadChannel(GAU_CH_FUEL, &raw_fuel);
+    ADC_ReadChannel(GAU_CH_COOLANT, &raw_coolant);
+    ADC_ReadChannel(GAU_CH_BATT, &raw_batt);
+    ADC_ReadChannel(GAU_CH_OIL, &raw_oil);
 
+    /* 1. Plausibility Check[cite: 1] */
+    uint8 fuel_ok = Check_Plausibility(raw_fuel, &ErrCount_Fuel);
+    uint8 cool_ok = Check_Plausibility(raw_coolant, &ErrCount_Coolant);
+    uint8 batt_ok = Check_Plausibility(raw_batt, &ErrCount_Batt);
+    uint8 oil_ok  = Check_Plausibility(raw_oil, &ErrCount_Oil);
+
+    if (fuel_ok && cool_ok && batt_ok && oil_ok) {
+        CarData->warnMask |= (1 << WARN_CHECK); /* Raise Check Engine Warning[cite: 1] */
+    } else {
+        CarData->warnMask &= ~(1 << WARN_CHECK); /* Note: 2-second hysteresis should be managed by scheduler[cite: 1] */
+    }
+
+    /* 2. Filtering for Fuel and Coolant (Moving Average of 8 samples)[cite: 1] */
+    Fuel_Buffer[Filter_Idx] = raw_fuel;
+    Coolant_Buffer[Filter_Idx] = raw_coolant;
+    Filter_Idx = (Filter_Idx + 1) % 8;
+    
+    for (i = 0; i < 8; i++) {
+        sum_fuel += Fuel_Buffer[i];
+        sum_coolant += Coolant_Buffer[i];
+    }
+    raw_fuel = sum_fuel / 8;
+    raw_coolant = sum_coolant / 8;
+
+    /* (Note: For simplicity, we assume Median for oil and battery takes the last 3 readings directly, you can build a buffer for it too) */
+
+    /* 3. Apply Scaling Equations using uint32 to avoid overflow[cite: 1] */
+    if (fuel_ok) {
+        CarData->fuelPct = (uint8)(((uint32)raw_fuel * 100) / 1023);
+    }
+    
+    if (cool_ok) {
+        CarData->coolantC = (uint16)((((uint32)raw_coolant * 170) / 1023) - 40);
+    }
+    
+    if (batt_ok) {
+        CarData->battmV = (uint16)(((uint32)raw_batt * 16000) / 1023);
+    }
+    
+    if (oil_ok) {
+        CarData->oilBarX10 = (uint8)(((uint32)raw_oil * 100) / 1023);
+    }
+}
